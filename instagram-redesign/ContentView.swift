a1f49cbf084b6e5288ac5f8119e2dd65781f6e95
Parineet <parineet.vernekar@gmail.com>
2021-07-27 22:10:54 +0100

    Initial Commit

---
+++ instagram-redesign/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  instagram-redesign
+//
+//  Created by Parineet Vernekar on 27/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ instagram-redesign/instagram_redesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_redesignApp.swift
+//  instagram-redesign
+//
+//  Created by Parineet Vernekar on 27/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct instagram_redesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
